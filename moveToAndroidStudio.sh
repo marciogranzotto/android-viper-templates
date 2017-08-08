cd2131beb87b2a9be2df9ec6a8efbc1613b83576
Marcio <marcio@ckl.io>
2017-08-08 11:00:00 -0300

    adds feedback on the move script

--- moveToAndroidStudio.sh
+++ moveToAndroidStudio.sh
@@ -1,1 +1,1 @@
-cp -r ViperTemplates /Applications/Android\ Studio.app/Contents/plugins/android/lib/templates/other/
+cp -r ViperTemplates /Applications/Android\ Studio.app/Contents/plugins/android/lib/templates/other/ && echo "Done!" || echo "Error!"
